c46302a65e2a1c0b597e5cf5091a9c9c7de2f68a
Johnny Lombardi <jlombardi004@gmail.com>
2022-04-01 14:52:15 -0700

    added GET & POST http request for product_api_project

--- products/views.py
+++ products/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products import serializers
+from .serializers import ProductSerializer
+from models import Product
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
-# Create your views here.
+
+from products.models import Product
+
+
+@api_view(['GET'])
+def products_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
